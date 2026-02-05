19bee35d2cd7b3105b4f310c73f9c4912ca7cb2d
AkshitaDiv3 <1262253339@mitwpu.edu.in>
2026-02-05 11:50:02 +0530

    Create Assignment8.c

---
+++ Assignment8.c
@@ -0,0 +1,20 @@
+#inculde<stdio.h>
+      void main()
+{ 
+  int orig , num , result=oij rem
+printf("Enter number:");
+scanf("%d", &num);
+num = orig;
+while(orig!=0)
+{
+num=orig%10;
+result=rem*rem*rem;
+orig=orig/10;
+}
+if(num==result)
+  printf("it is Armstrong");
+else
+  printf("Not Armstrong");
+}
+
+        
