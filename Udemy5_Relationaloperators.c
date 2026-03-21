49b354dff575187a02e3a74a6dafdf2555c57115
AkshitaDiv3 <1262253339@mitwpu.edu.in>
2026-03-21 22:31:27 +0530

    Create Udemy5_Relationaloperators.c

---
+++ Udemy5_Relationaloperators.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main();
+    int x = 20;
+    int y = 40;
+
+    printf("x = %d, y = %d\n\n", x, y);
+
+    printf("x == y: %d\n", x == y);
+
+    printf("x != y: %d\n", x != y);
+
+    printf("x > y: %d\n", x > y);
+
+    printf("x < y: %d\n", x < y);
+    
+    printf("x >= y: %d\n", x >= y);
+
+    printf("x <= y: %d\n", x <= y);
+
+return 0;
+}
